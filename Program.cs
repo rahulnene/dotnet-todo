c9addf3f5cd5fa6a65d9c76a22483c5b4a33b74c
Rahul Nene <rahul.nene@live.com>
2023-11-30 09:18:48 +0530

    Merged backend into frontend solution

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
